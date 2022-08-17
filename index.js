c99696613ccf17979ce04aef4886f1d08ef97c84
ElikanahMasanta <emasanta8@gmail.com>
2022-08-17 19:48:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(action ='roller-skate') {
+    return `This Saturday, I want to ${action}!`
+}
+saturdayFun(`This Saturday, I want to roller-skate!`)
+
+function mondayWork(action=`go to the office`) {
+    return `This Monday, I will ${action}.`
+}
+mondayWork(`This Monday, I will go to the office.`)
+
+function wrapAdjective(action =`a hard worker`) {
+    let result = wrapAdjective(action)
+      let emphatic = result(`a hard worker`)
+      return `You are ${action}!`
+}
+wrapAdjective(`You are, a hard worker`)
